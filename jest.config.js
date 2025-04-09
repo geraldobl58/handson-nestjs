d105a983d6d025551ecdd5428493e929efc0ad85
Geraldo Luiz <geraldobl58@gmail.com>
2025-04-09 17:47:29 -0300

    Update Jest configuration to support TypeScript testing and enhance module resolution. Upgrade dependencies for @types/jest and ts-jest to their latest versions for improved compatibility and performance.

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'cobertura'],
   testResultsProcessor: 'jest-sonar-reporter',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transformIgnorePatterns: ['/node_modules/'],
 };
