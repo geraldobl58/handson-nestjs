9e4367c2f3481b74984f9e80aaac11af1faa68fc
Geraldo Luiz <geraldobl58@gmail.com>
2025-04-09 17:45:13 -0300

    Refactor Jest configuration to enable coverage collection and update reporters. Introduce new test report files for improved analysis, including Cobertura and Sonar formats, while removing outdated coverage data.

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,6 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
-  coverageReporters: ['lcov', 'text-summary'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'coverage',
-        outputName: 'test-report.xml',
-        classNameTemplate: '{classname}',
-        titleTemplate: '{title}',
-        ancestorSeparator: ' > ',
-        suiteName: 'jest tests',
-        includeConsoleOutput: true,
-        usePathForSuiteName: true,
-      },
-    ],
-  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'cobertura'],
+  testResultsProcessor: 'jest-sonar-reporter',
 };
