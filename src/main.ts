e2a1348ad9bbdd0c0f1fc2cd8fda6ecdfa8be553
Geraldo Luiz <geraldobl58@gmail.com>
2025-04-11 18:08:32 -0300

    Remove Swagger dependencies and commented-out configuration from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,16 +13,16 @@ async function bootstrap() {
     res.status(200).send('OK');
   });
 
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('Hands-on NestJS API')
-    .setDescription('API documentation for Hands-on NestJS application')
-    .setVersion('1.0')
-    .addTag('users')
-    .addTag('auth')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // // Swagger configuration
+  // const config = new DocumentBuilder()
+  //   .setTitle('Hands-on NestJS API')
+  //   .setDescription('API documentation for Hands-on NestJS application')
+  //   .setVersion('1.0')
+  //   .addTag('users')
+  //   .addTag('auth')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
